d5ff6fd84f9ac91243b78c7a2ebfcfda815a0f7b
LuYF-Lemon-love <3555028709@qq.com>
2023-08-15 17:38:44 +0800

    修改代码

--- sample.py
+++ sample.py
@@ -19,12 +19,12 @@ from model import GPTConfig, GPT
 # -----------------------------------------------------------------------------
 init_from = 'resume'
 out_dir = 'out'
-start = "中国"
+start = "生命、宇宙和一切的答案是什么？"
 num_samples = 10
 max_new_tokens = 500
-temperature = 0.8
+temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
 top_k = 200
-seed = 1337
+seed = 42
 device = 'cuda'
 dtype = 'bfloat16' if torch.cuda.is_available() and torch.cuda.is_bf16_supported() else 'float16'
 compile = False
@@ -39,56 +39,37 @@ device_type = 'cuda' if 'cuda' in device else 'cpu'
 ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[dtype]
 ctx = nullcontext() if device_type == 'cpu' else torch.amp.autocast(device_type=device_type, dtype=ptdtype)
 
-if init_from == 'resume':
-    ckpt_path = os.path.join(out_dir, 'ckpt.pt')
-    checkpoint = torch.load(ckpt_path, map_location=device)
-    gptconf = GPTConfig(**checkpoint['model_args'])
-    model = GPT(gptconf)
-    state_dict = checkpoint['model']
-    unwanted_prefix = '_orig_mod.'
-    for k,v in list(state_dict.items()):
-        if k.startswith(unwanted_prefix):
-            state_dict[k[len(unwanted_prefix):]] = state_dict.pop(k)
-    model.load_state_dict(state_dict)
-elif init_from.startswith('gpt2'):
-    # init from a given GPT-2 model
-    model = GPT.from_pretrained(init_from, dict(dropout=0.0))
+ckpt_path = os.path.join(out_dir, 'ckpt.pt')
+checkpoint = torch.load(ckpt_path, map_location=device)
+gptconf = GPTConfig(**checkpoint['model_args'])
+model = GPT(gptconf)
+state_dict = checkpoint['model']
+unwanted_prefix = '_orig_mod.'
+for k,v in list(state_dict.items()):
+    if k.startswith(unwanted_prefix):
+        state_dict[k[len(unwanted_prefix):]] = state_dict.pop(k)
+model.load_state_dict(state_dict)
 
 model.eval()
 model.to(device)
 if compile:
     model = torch.compile(model)
 
-load_meta = False
-if init_from == 'resume' and 'config' in checkpoint and 'dataset' in checkpoint['config']:
-    meta_path = os.path.join('data', checkpoint['config']['dataset'], 'meta.pkl')
-    load_meta = os.path.exists(meta_path)
-if load_meta:
-    print(f"Loading meta from {meta_path}...")
-    with open(meta_path, 'rb') as f:
-        meta = pickle.load(f)
-    # TODO want to make this more general to arbitrary encoder/decoder schemes
-    stoi, itos = meta['stoi'], meta['itos']
-    encode = lambda s: [stoi[c] for c in s]
-    decode = lambda l: ''.join([itos[i] for i in l])
-else:
-    print("加载分词器...")
-    class CustomDecoder:
-        def decode_chain(self, tokens: List[str]) -> List[str]:
-            return [f"{t}" for t in tokens]
-    tokenizer = Tokenizer.from_file("data/wiki_zh_2019/tokenizer.json")
-    tokenizer.decoder = Decoder.custom(CustomDecoder())
-    encode = lambda s: tokenizer.encode(s).ids
-    decode = lambda l: tokenizer.decode(l)
+print("加载分词器...")
+class CustomDecoder:
+    def decode_chain(self, tokens: List[str]) -> List[str]:
+        return [f"{t}" for t in tokens]
+tokenizer = Tokenizer.from_file(os.path.join('data', checkpoint['config']['dataset'], "data/wiki_zh_2019/tokenizer.json"))
+tokenizer.decoder = Decoder.custom(CustomDecoder())
+encode = lambda s: tokenizer.encode(s).ids
+decode = lambda l: tokenizer.decode(l)
 
-# encode the beginning of the prompt
 if start.startswith('FILE:'):
     with open(start[5:], 'r', encoding='utf-8') as f:
         start = f.read()
 start_ids = encode(start)
 x = (torch.tensor(start_ids, dtype=torch.long, device=device)[None, ...])
 
-# run generation
 with torch.no_grad():
     with ctx:
         for k in range(num_samples):
